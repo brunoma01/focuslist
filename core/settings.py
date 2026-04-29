048a5c96d0211cc8cf80cf724b32d6ce78bc1e3e
brunoma01 <brunomachadoalmeida01@gmail.com>
2026-04-29 18:46:52 -0300

    fix: debug false

--- core/settings.py
+++ core/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_-4!i!echx_n7ul4fh!5c%aa$z78zxe-(3f69=0lckjw1bgpaz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
