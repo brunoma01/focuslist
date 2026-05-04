604cb4daa43a363ad103e6d9c01ce691829d9fba
brunoma01 <brunomachadoalmeida01@gmail.com>
2026-05-04 14:19:13 -0300

    feat: add first_name field and display user name properly

--- tarefas/forms.py
+++ tarefas/forms.py
@@ -1,7 +1,27 @@
 from django import forms
 from .models import Task
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['label', 'due_date']
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'password1', 'password2']
+
+def clean_first_name(self):
+    name = self.cleaned_data.get('first_name')
+    return name.capitalize()
+
+def save(self, commit=True):
+    user = super().save(commit=False)
+    user.first_name = self.cleaned_data['first_name']
+    if commit:
+        user.save()
+    return user
